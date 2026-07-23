027cced5426e5afbeeb19897633513a461058e73
mamirirom-png <m.amirirom@gmail.com>
2026-07-23 13:56:21 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-.
+document.addEventListener("DOMContentLoaded",()=>{
+
+const cards=document.querySelectorAll(".card,.feature");
+
+const observer=new IntersectionObserver((entries)=>{
+
+entries.forEach(entry=>{
+
+if(entry.isIntersecting){
+
+entry.target.style.opacity="1";
+entry.target.style.transform="translateY(0)";
+}
+
+});
+
+},{threshold:0.15});
+
+cards.forEach(card=>{
+
+card.style.opacity="0";
+card.style.transform="translateY(50px)";
+card.style.transition="all .8s ease";
+
+observer.observe(card);
+
+});
+
+});
